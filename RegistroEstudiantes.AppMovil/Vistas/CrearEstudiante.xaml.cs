6c9c34a3cad0d60a2a095727cd7b2401448a1345
FelipeCastro <felipe.castro96@inacapmail.cl>
2024-12-10 02:37:42 -0300

    Lista cursos y guarda estudiantes

--- RegistroEstudiantes.AppMovil/Vistas/CrearEstudiante.xaml.cs
+++ RegistroEstudiantes.AppMovil/Vistas/CrearEstudiante.xaml.cs
@@ -7,10 +7,19 @@ namespace RegistroEstudiantes.AppMovil.Vistas;
 public partial class CrearEstudiante : ContentPage
 {
     FirebaseClient client = new FirebaseClient("https://estudiantes-4a646-default-rtdb.firebaseio.com/");
+
+    public List<Curso> Cursos { get; set; }
     public CrearEstudiante()
     {
         InitializeComponent();
+        ListarCursos();
         BindingContext = this;
+    }
+
+    private void ListarCursos()
+    {
+        var cursos = client.Child("Cursos").OnceAsync<Curso>();
+        Cursos = cursos.Result.Select(x=>x.Object).ToList();
     }
 
     private async void guardarButton_Clicked(object sender, EventArgs e)
@@ -26,10 +35,16 @@ public partial class CrearEstudiante : ContentPage
             Curso = curso
         };
 
-        await client.Child("Estudiantes").PostAsync(estudiante);
-
-        await DisplayAlert("Éxito", $"El Estudiante {estudiante.Nombre} {estudiante.Apellido} fue guardado correctamente", "OK");
-
-        await Navigation.PopAsync();
+        try
+        {
+            await client.Child("Estudiantes").PostAsync(estudiante);
+            await DisplayAlert("Éxito", $"El Estudiante {estudiante.Nombre} {estudiante.Apellido} fue guardado correctamente", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message,"OK");
+        }
+           
     }
 }
